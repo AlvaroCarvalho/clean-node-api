435355a71d8d62a869d022b691829023f443331d
AlvaroCarvalho <alvaro.azeredo@hotmail.com>
2020-07-29 12:45:31 -0300

    test: ensure AddAccountToRepositoy throws if encrypter throws

--- src/data/usecases/AddAccount/AddAccountToRepository.spec.ts
+++ src/data/usecases/AddAccount/AddAccountToRepository.spec.ts
@@ -36,4 +36,17 @@ describe('AddAccountToRepository', () => {
     await sut.add(accountData)
     expect(spy).toBeCalledWith(accountData.password)
   })
+
+  test('Should throw if encrypter throws', async () => {
+    const { sut, encrypterStub } = makeSut()
+    jest.spyOn(encrypterStub, 'encrypt').mockReturnValueOnce(new Promise((resolve, reject) => reject(new Error())))
+    const accountData = {
+      name: 'valid_name',
+      email: 'valid_email',
+      password: 'valid_password'
+    }
+
+    const promise = sut.add(accountData)
+    await expect(promise).rejects.toThrow()
+  })
 })
