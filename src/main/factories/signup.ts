394756530843c171585914f59a94500df5083796
AlvaroCarvalho <alvaro.azeredo@hotmail.com>
2020-08-06 14:06:49 -0300

    refactor: move sut creation to a factory helper method

--- src/main/factories/signup.ts
+++ src/main/factories/signup.ts
@@ -1,9 +1,9 @@
-
 import { SignUpController } from '../../presentation/controllers/signup/signup'
 import { EmailValidatorAdapter } from '../../utils/EmailValidatorAdapter'
 import { AddAccountToDatabase } from '../../data/usecases/AddAccount/AddAccountToDatabase'
 import { BcryptAdapter } from '../../infra/criptography/BcryptAdapter'
 import { AccountMongoRepository } from '../../infra/db/mongodb/account/account'
+import { LogMongoRepository } from '../../infra/db/mongodb/log/log'
 import { LogControllerDecorator } from '../decorators/log'
 import { Controller } from '../../presentation/protocols/controller'
 
@@ -15,5 +15,6 @@ export const makeSignUpController = (): Controller => {
   const emailValidatorAdapter = new EmailValidatorAdapter()
   const addAccountToDatabase = new AddAccountToDatabase(bcryptAdapter, accountMongoRepository)
   const signUpController = new SignUpController(emailValidatorAdapter, addAccountToDatabase)
-  return new LogControllerDecorator(signUpController)
+  const logMongoRepository = new LogMongoRepository()
+  return new LogControllerDecorator(signUpController, logMongoRepository)
 }
